02b9f33a05f46f00f38d5707a161cecb3f3e3530
Alexander Klingenbeck <mode7@gmx.net>
2019-10-03 12:36:24 +0200

    add typescript

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/main.ts',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: './dist/[name].bundle.js',
+    path: __dirname
+  }
+};
